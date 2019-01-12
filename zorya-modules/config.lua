9204e0147b105f89761564bad9a862645ff26cfe
Adorable-Catgirl <janeroxanne0@gmail.com>
2019-01-12 10:58:26 -0500

    Create config.lua

---
+++ zorya-modules/config.lua
@@ -0,0 +1,32 @@
+local args = {...}
+local envs = args[1]
+
+local component = component or require("component")
+
+--envs.boot[#envs.boot+1] = {"Zorya BIOS configuration", "cfg", {}}
+
+envs.hand["cfg"] = function(args)
+	return envs.loadfile(envs.device, "zorya-cfg/zorya-cfg.lua")(envs, args)
+end
+
+--Just check for our config files. Also, this file is loaded second to
+--last, so we can both make a .zoryarc and/or replace the boot list.
+local json, err = envs.loadfile(envs.device, "zorya-cfg/json.lua")
+local pretty, err = envs.loadfile(envs.device, "zorya-cfg/pretty.lua")
+if not json then error(err) else json = json() end
+if not pretty then error(err) else pretty = pretty() end
+
+local fs = component.proxy(envs.device)
+
+if (not fs.exists("zorya-cfg/.zoryarc")) then
+	envs.boot[#envs.boot+1] = {"Update Zorya and Init Modules", "netboot", "https://raw.githubusercontent.com/Adorable-Catgirl/Zorya-BIOS/master/update/init.lua", {}}
+	local hand = fs.open("zorya-cfg/.zoryarc", "w")
+	local str = pretty(envs.boot, "\n", "\t", " ", json.encode)
+	fs.write(hand, str)
+	fs.close(hand)
+else
+	local hand = fs.open("zorya-cfg/.zoryarc", "r")
+	local dat = fs.read(hand, math.huge)
+	envs.boot = json.decode(dat)
+	fs.close(hand, str)
+end
