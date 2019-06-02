98a9f42c93427b1ced2e485a4b3abc79128cbaa3
Jane Roxanne <janeroxanne0@gmail.com>
2019-06-02 01:18:23 -0500

    made it not bsod

--- zorya-modules/97_setres.lua
+++ zorya-modules/97_setres.lua
@@ -6,5 +6,7 @@ envs.scan[#envs.scan+1] = function(envs)
 end
 
 envs.set[#envs.set+1] = function(envs)
-	envs.gpu.setViewport(envs.cfg.gpuset.x, envs.cfg.gpuset.y)
+	if (envs.cfg.gpuset) then
+		envs.gpu.setViewport(envs.cfg.gpuset.x, envs.cfg.gpuset.y)
+	end
 end
