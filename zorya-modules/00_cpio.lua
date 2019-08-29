cab164e3af233009021db442a2e66ff24324610f
Jane Roxanne <janeroxanne0@gmail.com>
2019-08-29 09:38:02 -0500

    Let's go.

--- zorya-modules/00_cpio.lua
+++ zorya-modules/00_cpio.lua
@@ -1,30 +1,39 @@
+
 zorya.cpio = {}
-local comp = component or require("component")
+local comp = component
+local component = comp
+local bit32 = bit32
+
+
+local function readint(fs, handle, amt, rev)
+    local tmp = 0
+    for i=(rev and amt) or 1, (rev and 1) or amt, (rev and -1) or 1 do
+    	tmp = bit32.bor(tmp, bit32.lshift(fs.read(handle, 1):byte(), ((i-1)*8)))
+    end
+    return tmp
+end
 
 --- Returns true if the file is a CPIO. False otherwises
 function zorya.cpio.validate(fs, path)
-	fs = comp.proxy(fs)
+	local fs = comp.proxy(fs)
 	local handle = fs.open(path, "r")
-	local function readint(amt, rev)
-	    local tmp = 0
-	    for i=(rev and amt) or 1, (rev and 1) or amt, (rev and -1) or 1 do
-	    	tmp = bit32.bor(tmp, bit32.lshift(fs.read(handle, 1):byte(), ((i-1)*8)))
-	    end
-	    return tmp
-	end
-	local magic = readint(2)
+	local magic = readint(fs, handle, 2)
 	if (magic == tonumber("070707", 8)) then
 		return true
 	end
+	fs.close(handle)
 	return false
 end
 
---- Load the cpio into a table. If value is true, then key is a directory, otherwise value is a string and key corresponds
---- to a file
-function zorya.cpio.load(fs, path)
-	fs = comp.proxy(fs)
-	local cpio = {}
-	local handle = fs.open(path, "r")
+local cpio = {}
+
+local function cpio_index(self, file)
+	local fs = self.__fs
+	local handle = self.__handle
+	fs.seek(hadnle, "set", 0)
+	if (cpio[file]) then
+		return cpio[file]
+	end
 	local dent = {
 	    magic = 0,
 	    dev = 0,
@@ -38,44 +47,63 @@ function zorya.cpio.load(fs, path)
 	    namesize = 0,
 	    filesize = 0,
 	}
-	local function readint(amt, rev)
-	    local tmp = 0
-	    for i=(rev and amt) or 1, (rev and 1) or amt, (rev and -1) or 1 do
-	    	tmp = bit32.bor(tmp, bit32.lshift(fs.read(handle, 1):byte(), ((i-1)*8)))
-	    end
-	    return tmp
-	end
+	local found, dir
 	while true do
-		dent.magic = readint(2)
+		dent.magic = readint(fs, handle, 2)
 		local rev = false
 	    if (dent.magic ~= tonumber("070707", 8)) then rev = true end
-	    dent.dev = readint(2)
-	    dent.ino = readint(2)
-	    dent.mode = readint(2)
-	    dent.uid = readint(2)
-	    dent.gid = readint(2)
-	    dent.nlink = readint(2)
-	    dent.rdev = readint(2)
-	    dent.mtime = bit32.bor(bit32.lshift(readint(2), 16), readint(2))
-	    dent.namesize = readint(2)
-	    dent.filesize = bit32.bor(bit32.lshift(readint(2), 16), readint(2))
+	    dent.dev = readint(fs, handle, 2)
+	    dent.ino = readint(fs, handle, 2)
+	    dent.mode = readint(fs, handle, 2)
+	    dent.uid = readint(fs, handle, 2)
+	    dent.gid = readint(fs, handle, 2)
+	    dent.nlink = readint(fs, handle, 2)
+	    dent.rdev = readint(fs, handle, 2)
+	    dent.mtime = bit32.bor(bit32.lshift(readint(fs, handle, 2), 16), readint(fs, handle, 2))
+	    dent.namesize = readint(fs, handle, 2)
+	    dent.filesize = bit32.bor(bit32.lshift(readint(fs, handle, 2), 16), readint(fs, handle, 2))
 	    local name = fs.read(handle, dent.namesize):sub(1, dent.namesize-1)
 	    if (name == "TRAILER!!!") then break end
 	    dent.name = name
 	    if (dent.namesize % 2 ~= 0) then
 	        fs.read(handle, 1) -- skip one byte
 	    end
+	    if (bit32.band(dent.mode, 16384) ~= 0) then
+	    	found = true
+	    	dir = true
+	    	break
+	    end
 	    if bit32.band(dent.mode, 32768) ~= 0 then -- just wait for Lua 5.3 compatibility!
-	        local dir = dent.name:match("(.+)/.*%.?.+")
-	        if (dir) then
-	        	cpio[dir] = true
-	        end
-	        cpio[dent.name] = fs.read(handle, dent.filesize) -- TODO: use better method to read files over 2KB
+	        --local dir = dent.name:match("(.+)/.*%.?.+")
+	        found = true
+	        break
 	    end
 	    if (dent.filesize % 2 ~= 0) then
 	        fs.read(handle, 1) -- skip one byte
 	    end
 	end
-	fs.close(handle)
-	return cpio
+	if not found then
+		return nil, "file not found"
+	end
+	if dir then
+		return dir
+	end
+	local dat = ""
+	local fz = dent.filesize
+	while true do
+		dat = dat .. fs.read(handle, fz)
+		fz = fz - #dat
+		if (fz < 1) then
+			break
+		end
+	end
+	return dat
+end
+
+
+--- Load the cpio into a table. If value is true, then key is a directory, otherwise value is a string and key corresponds
+--- to a file
+function zorya.cpio.load(fs, path)
+	local arc = {}
+	return setmetatable({__fs=component.proxy(fs), __handle=fs.open(path, "r")}, {__index=cpio_index})
 end
---
+++ zorya-modules/96_biosdev.lua
@@ -0,0 +1,15 @@
+local function mount_cpio(drive, path)
+	
+end
+
+local function zlan_wake(key)
+
+end
+
+local function zlan_enable()
+
+end
+
+local function zlan_disable()
+
+end
